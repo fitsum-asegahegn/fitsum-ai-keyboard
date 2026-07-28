3903e695649cf249449ef23a5dab7fd800c33e01
fitsum-asegahegn <fitsumasegahegn@gmail.com>
2026-07-28 03:00:17 +0300

    Update AmharicImeService.kt

--- app/src/main/java/com/fitsum/aikeyboard/AmharicImeService.kt
+++ app/src/main/java/com/fitsum/aikeyboard/AmharicImeService.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputConnection
 import android.widget.LinearLayout
-import com.ethiopia.keyboard.R
+import com.fitsum.aikeyboard.R
 import com.fitsum.aikeyboard.GeEzTransliterationEngine
 import com.fitsum.aikeyboard.CandidateViewManager
 
